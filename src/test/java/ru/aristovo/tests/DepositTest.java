5a1c7fd1636c6e147ecfc74b8f9d16b4c121775d
Oleg Pushkarev <tyler98765@yandex.ru>
2021-02-24 14:43:45 +0300

    Добавлен класс DepositTest - заготовка теста

---
+++ src/test/java/ru/aristovo/tests/DepositTest.java
@@ -0,0 +1,12 @@
+package ru.aristovo.tests;
+
+import org.junit.Test;
+import ru.aristovo.base.BaseTests;
+
+public class DepositTest extends BaseTests {
+
+    @Test
+    public void depositCalculateTest() {
+
+    }
+}
