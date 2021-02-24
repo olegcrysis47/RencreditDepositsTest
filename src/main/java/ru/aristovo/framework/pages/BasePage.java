1409af38327efe523b6de25051816f5377436770
Oleg Pushkarev <tyler98765@yandex.ru>
2021-02-24 15:02:37 +0300

    Заговлен базовый класс страниц сайта BasePage с основными методами

--- src/main/java/ru/aristovo/framework/pages/BasePage.java
+++ src/main/java/ru/aristovo/framework/pages/BasePage.java
@@ -1,4 +1,154 @@
 package ru.aristovo.framework.pages;
 
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import ru.aristovo.framework.managers.PageManager;
+import ru.aristovo.framework.managers.TestPropManager;
+
+import java.util.concurrent.TimeUnit;
+
+import static ru.aristovo.framework.managers.DriverManager.getDriver;
+import static ru.aristovo.framework.utils.PropConst.IMPLICITLY_WAIT;
+
+/**
+ * Базовый класс всех страничек
+ */
 public class BasePage {
+
+    /**
+     * Менеджер страничек
+     *
+     * @see PageManager
+     */
+    protected PageManager app = PageManager.getPageManager();
+
+    /**
+     * Объект для имитации реального поведения мыши или клавиатуры
+     *
+     * @see Actions
+     */
+    protected Actions action = new Actions(getDriver());
+
+    /**
+     * Объект для выполнения любого js кода
+     *
+     * @see JavascriptExecutor
+     */
+    protected JavascriptExecutor js = (JavascriptExecutor) getDriver();
+
+    /**
+     * Объект явного ожидания
+     * При применении будет ожидать заданного состояния 10 секунд с интервалом в 1 секунду
+     *
+     * @see WebDriverWait
+     */
+    protected WebDriverWait wait = new WebDriverWait(getDriver(), 10, 1000);
+
+    /**
+     * Менеджер properties
+     *
+     * @see TestPropManager#getTestPropManager()
+     */
+    private final TestPropManager props = TestPropManager.getTestPropManager();
+
+    /**
+     * Конструктор позволяющий инициализировать все странички и их элементы помеченные аннотацией {@link FindBy}
+     * Подробнее можно просмотреть в класс {@link org.openqa.selenium.support.PageFactory}
+     *
+     * @see FindBy
+     * @see PageFactory
+     * @see PageFactory#initElements(WebDriver, Object)
+     */
+    public BasePage() {
+        PageFactory.initElements(getDriver(), this);
+    }
+
+    /**
+     * Функция позволяющая производить scroll до любого элемента с помощью js
+     *
+     * @param element - веб-элемент странички
+     * @see JavascriptExecutor
+     */
+    protected void scrollToElementJs(WebElement element) {
+        js.executeScript("arguments[0].scrollIntoView(true);", element);
+    }
+
+    /**
+     * Функция позволяющая производить scroll до любого элемента с помощью js со смещение
+     * Смещение задается количеством пикселей по вертикали и горизонтали, т.е. смещение до точки (x, y)
+     *
+     * @param element - веб-элемент странички
+     * @param x       - параметр координаты по горизонтали
+     * @param y       - параметр координаты по вертикали
+     * @see JavascriptExecutor
+     */
+    public void scrollWithOffset(WebElement element, int x, int y) {
+        String code = "window.scroll(" + (element.getLocation().x + x) + ","
+                + (element.getLocation().y + y) + ");";
+        ((JavascriptExecutor) getDriver()).executeScript(code, element, x, y);
+    }
+
+    /**
+     * Явное ожидание состояния clickable элемента
+     *
+     * @param element - веб-элемент который требует проверки clickable
+     * @return WebElement - возвращаем тот же веб элемент что был передан в функцию
+     * @see WebDriverWait
+     * @see org.openqa.selenium.support.ui.FluentWait
+     * @see org.openqa.selenium.support.ui.Wait
+     * @see ExpectedConditions
+     */
+    protected WebElement elementToBeClickable(WebElement element) {
+        return wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    /**
+     * Метод ожидания видимости элемента на странице.
+     * @param locator - локатор элемента.
+     */
+    private void waitUtilElementToBeVisible(By locator) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    /**
+     * Метод ожидания видимости элемента на странице.
+     * @param element - веб-элемент на странице сайта.
+     */
+    private void waitUtilElementToBeVisible(WebElement element) {
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    /**
+     * @param by - Объект задающий локатор поиска {@link By}
+     * @return boolean - true если элемент присутствует, false если элемент отсутствует
+     */
+    public boolean isElementExist(By by) {
+        boolean flag = false;
+        try {
+            getDriver().manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+            getDriver().findElement(by);
+            flag = true;
+        } catch (NoSuchElementException ignore) {
+        } finally {
+            getDriver().manage().timeouts().implicitlyWait(Integer.parseInt(props.getProperty(IMPLICITLY_WAIT)), TimeUnit.SECONDS);
+        }
+        return flag;
+    }
+
+    // ---------------------Мои методы---------------------
+    /**
+     * Метод, чтобы задержать тест, если драйвер не будет успевать отрабатывать.
+     * @param time - время в миллисекундах.
+     */
+    public void waitThread(int time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
