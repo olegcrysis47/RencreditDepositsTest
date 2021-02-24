4216a4d42193d812f543f70f72dea34393ab7327
Oleg Pushkarev <tyler98765@yandex.ru>
2021-02-24 14:24:29 +0300

    Добавлен класс инициализации framework - InitManager

---
+++ src/main/java/ru/aristovo/framework/managers/InitManager.java
@@ -0,0 +1,39 @@
+package ru.aristovo.framework.managers;
+
+import java.util.concurrent.TimeUnit;
+
+import static ru.aristovo.framework.managers.DriverManager.*;
+import static ru.aristovo.framework.utils.PropConst.*;
+
+/**
+ * Класс для инициализации фреймворка
+ */
+public class InitManager {
+
+    /**
+     * Менеджер пропертей
+     * @see TestPropManager#getTestPropManager()
+     */
+    public static TestPropManager props = TestPropManager.getTestPropManager();
+
+    /**
+     * Инициализация фреймворка и запуск браузера со страницей приложения
+     * @see DriverManager#getDriver()
+     * @see TestPropManager#getProperty(String)
+     * @see ru.aristovo.framework.utils.PropConst
+     */
+    public static void initFramework() {
+        getDriver().manage().window().maximize();
+        getDriver().manage().timeouts().implicitlyWait(Integer.parseInt(props.getProperty(IMPLICITLY_WAIT)), TimeUnit.SECONDS);
+        getDriver().manage().timeouts().pageLoadTimeout(Integer.parseInt(props.getProperty(PAGE_LOAD_TIMEOUT)), TimeUnit.SECONDS);
+        getDriver().get(props.getProperty(APP_URL));
+    }
+
+    /**
+     * Завершения работы фреймворка - гасит драйвер и закрывает сессию с браузером
+     * @see DriverManager#quitDriver()
+     */
+    public static void quitFramework() {
+        quitDriver();
+    }
+}
